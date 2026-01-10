87cea62431d3e2a87461ced019cfe298597d9885
Marek <marek.j.swakon@gmail.com>
2026-01-10 20:27:33 +0100

    siwydym

--- detection_test.py
+++ detection_test.py
@@ -1,6 +1,6 @@
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import time
+import cv2
 
 camera = cv2.VideoCapture(0)
 caffe_model = 'models/Res10_300x300_ssd_iter_140000.caffemodel'
@@ -11,7 +11,7 @@ THRESHOLD = 0.5
 
 
 
-def adjusted_detect_face(frame):
+def detect_face(frame):
     (h, w) = frame.shape[:2]
 
     # 1. Pre-process the frame: resize to 300x300 and subtract mean RGB values
@@ -24,6 +24,8 @@ def adjusted_detect_face(frame):
 
     best_confidence = THRESHOLD
     best_detection = None
+    face_rect = None
+    confidence = 0.0
 
     # 3. Loop over the detections
     for i in range(0, detections.shape[2]):
@@ -32,10 +34,10 @@ def adjusted_detect_face(frame):
         if confidence > best_confidence:
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")
+            face_rect = ([startX, startY, endX, endY])
+            best_confidence = confidence
 
-            best_detection = ([startX, startY, endX, endY, confidence])
-
-    return best_detection
+    return face_rect, confidence
 
 def capture_frame():
     ret, frame = camera.read()
@@ -43,27 +45,31 @@ def capture_frame():
         raise RuntimeError("Failed to capture image from camera.")
     return frame
 
-def draw_frame(frame,face_rect):
-    if not face_rect:
+def draw_frame(frame, face_rect, confidence):
+    if face_rect is None:
         cv2.imshow("Face Detection", frame)
-        return frame
-    (startX, startY, endX, endY, conf)= face_rect
+        return
+
+    startX, startY, endX, endY = face_rect
 
     cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
 
-    # Draw the confidence label
-    text = f"{conf * 100:.2f}%"
+    text = f"{confidence * 100:.2f}%"
     y = startY - 10 if startY - 10 > 10 else startY + 10
     cv2.putText(frame, text, (startX, y),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+
     cv2.imshow("Face Detection", frame)
-    return frame
+
 
 if __name__ == "__main__":
     while True:
         frame = capture_frame()
-        face_rect = adjusted_detect_face(frame)
-        frame_with_detections = draw_frame(frame, face_rect)
+        current_time = time.time()
+        face_rect, confidence = detect_face(frame)
+        draw_frame(frame, face_rect, confidence)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     camera.release()
+    cv2.destroyAllWindows()
+
